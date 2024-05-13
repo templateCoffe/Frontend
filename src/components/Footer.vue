67024664a443c09d2c540ad903f8a47d30ac1245
alex <alexander.gonzalez1@utp.edu.co>
2024-05-13 10:54:14 -0500

    actulizacion menu

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="Footer">
+        test
+    </div>
+</template>
+
+<style scoped>
+.Footer {
+    grid-area: Footer;
+    background-color: black;
+    bottom: 0;
+    height: 40px;
+}
+</style>
--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,5 +1,6 @@
 <script setup>
   import Header from './Header.vue';
+  import Footer from './Footer.vue';
     const menu_cafe=[
       {
         id: "1",
@@ -24,7 +25,7 @@
 
 <template>
   <Header />
-  <div class="row my-3">
+  <!--<div class="row my-3">
     <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
       <table>
         <thead>
@@ -40,35 +41,66 @@
         </tbody>
       </table>
     </div>
-</div>
+</div>-->
+<div class="row my-3">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+      <div id = "cabecera">
+
+              <a><button type="button" class="TableButtons"> Bebidas Calientes  </button></a>
+              <a><button type="button" class="TableButtons"> Bebidas Frias  </button></a>
+              <a><button type="button" class="TableButtons"> Bebidas con Licor  </button></a>
+              <a><button type="button" class="TableButtons"> Postres artesanales  </button></a>
+              <a><button type="button" class="TableButtons">Especialidades</button></a>
+        </div>
+        <div id = cuerpo>
+          <p>este es el cuerpo</p>
+
+
+        </div>
+      </div>
+  </div>
+<Footer />
 </template>
 
 <style>
 
-  table{
+  /* table{
     background-color: #A6A2A2;
     color: rgb(255, 255, 255);
     margin: 0px auto;
-  }
-  .TableButtons {
+  }*/
 
+  #cabecera {    
+padding: 19px;
+background-color: #A6A2A2 ;
+width: 70%;
+margin: 0px auto;
+}
+#cuerpo {
+background-color: #E5E6E4;    
+border:3px solid #A6A2A2;
+padding: 19px;
+width: 70%;
+margin: 0px auto;
+border-bottom-right-radius: 5px;
+border-bottom-left-radius: 5px;
+}
+
+  .TableButtons {
+display: inline-block;
 color: #FBFBF2;
 font-size: 20px;
 background-color: transparent;
-padding-left: 10px;
-margin: 20px;
+margin-left: 4%;
 border: none;
-display: absolute;
 text-align: center;
 cursor: pointer;
-width: 100%;
 }
 
 .TableButtons:hover {
 
 transition: 0ms;
-padding-left: 10px;
-transform: translateX(-20px);
+transform: translateX(0px);
 background-color: #E5E6E4;
 color:  #A6A2A2;
 border: none;

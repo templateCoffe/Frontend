d72554947a09ebe5169d8cf6522d4c855f944874
Juan Alejandro Pérez Bermúdez <a.perez3@utp.edu.co>
2024-05-19 15:06:08 -0500

    feat: easy integration of google reviews

--- src/components/Comentarios.vue
+++ src/components/Comentarios.vue
@@ -1,3 +1,44 @@
 <template>
-    
+    <Header />
+    <div class="Place">
+        <h1>Deja tus comentarios</h1>
+        <div class="elfsight-app-2adf9dd9-c072-4aaa-bb4b-c6f88d7cb15b" data-elfsight-app-lazy></div>
+    </div>
+    <Footer />
 </template>
+
+<script setup>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import { onMounted, ref } from 'vue';
+
+const elfsightContainer = ref(null);
+
+onMounted(() => { //Mecanismo interesante para incluir scripts externos en el código
+    const script = document.createElement('script');
+    script.src = "https://static.elfsight.com/platform/platform.js";
+    script.setAttribute('data-use-service-core', '');
+    script.defer = true;
+    script.onload = () => {
+        if (window.ElsWidget) {
+            window.ElsWidget.init();
+        } else {
+            document.addEventListener('ElsWidgetInit', () => {
+                window.ElsWidget.init();
+            });
+        }
+    };
+
+    document.body.appendChild(script);
+});
+</script>
+
+<style>
+.Place {
+    padding: 100px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 50px;
+}
+</style>

0b03ff4f0375acdb325154960d66a9a98b8d9580
Juan Alejandro Pérez Bermúdez <a.perez3@utp.edu.co>
2024-05-19 11:59:20 -0500

    feat: non several changes in Admin.vue

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,3 +1,9 @@
 <template>
-    
+    <div class="lista-de-botones">
+        <router-link to="administrador_menu"><button type="button">Menu</button></router-link>
+        <router-link><button type="button">Reservas</button></router-link>
+        <router-link><button type="button">Blog</button></router-link>
+        <router-link><button type="button">Chatbot</button></router-link>
+        <router-link><button type="button">Inventario</button></router-link>
+    </div>
 </template>

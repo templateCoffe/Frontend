27576f04ee038ffe077a53c6937592ca2ace0e35
Juan Alejandro Pérez Bermúdez <a.perez3@utp.edu.co>
2024-05-19 08:53:14 -0500

    feat: login base created

--- src/main.js
+++ src/main.js
@@ -1,25 +1,9 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import './style_menu.css'
 import App from './App.vue'
-import Index from './components/Index.vue'
-import Menu from './components/Menu.vue'
-import Reservas from './components/Reservas.vue'
-import Comentarios from './components/Comentarios.vue'
-import Blog from './components/Blog.vue'
 import './assets/css/bootstrap.min.css'
 import './assets/js/bootstrap.bundle.min.js'
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', component: Index },
-        { path: '/reservas', component: Reservas },
-        { path: '/menu', component: Menu },
-        { path: '/comentarios', component: Comentarios },
-        { path: '/blog', component: Blog},
-    ]
-});
+import router from './router.js'
 
 createApp(App).use(router).mount('#app')
---
+++ src/router.js
@@ -0,0 +1,24 @@
+import Index from './components/Index.vue'
+import Menu from './components/Menu.vue'
+import Reservas from './components/Reservas.vue'
+import Comentarios from './components/Comentarios.vue'
+import Blog from './components/Blog.vue'
+import Login from './components/Login.vue'
+import Admin from './components/Admin.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: Index },
+        { path: '/reservas', component: Reservas },
+        { path: '/menu', component: Menu },
+        { path: '/comentarios', component: Comentarios },
+        { path: '/blog', component: Blog },
+        { path: '/login', component: Login },
+        { path: '/admin', component: Admin },
+
+    ]
+});
+
+export default router;

51f76f57799f55a1202c7a2608e19c834db22ec7
Juan Alejandro Pérez Bermúdez <a.perez3@utp.edu.co>
2024-05-27 22:42:13 -0500

    fix: logout filename ¬¬

---
+++ src/components/subComponents/logout.vue
@@ -0,0 +1,28 @@
+// Navbar.vue
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li v-if="!isAuthenticated"><router-link to="/login">Login</router-link></li>
+      <li v-else><a href="#" @click="logout">Logout</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import { isAuthenticated, logout } from './auth';
+
+export default {
+  computed: {
+    isAuthenticated() {
+      return isAuthenticated();
+    }
+  },
+  methods: {
+    logout() {
+      logout();
+      this.$router.push('/login');
+    }
+  }
+};
+</script>

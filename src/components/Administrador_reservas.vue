cb9a0688fb4f22a4edff812bd5a74a26fc7904b3
Juan Alejandro Pérez Bermúdez <a.perez3@utp.edu.co>
2024-06-01 09:16:16 -0500

    feat: administrador de reservas va tomando forma

--- src/components/Administrador_reservas.vue
+++ src/components/Administrador_reservas.vue
@@ -1,77 +1,124 @@
 <script setup>
-import Header from './Header.vue';
-import Footer from './Footer.vue';
-import Pendientes from './subComponents/Pendientes.vue';
-import Procesados from './subComponents/Procesados.vue';
-import {ref} from 'vue'
-let op = 1
-let change = ref(1)
-const opciones = (op) => {
-  if(op==1){
-    change.value = 1
-      }
-    else if(op==2){
-        change.value = 2
-      }
-      
-    }
-
+import Footer from "./Footer.vue";
+import AdminReservasFrame from "./subComponents/AdminReservasFrame.vue";
+import Pendientes from "./subComponents/Pendientes.vue";
+import Procesados from "./subComponents/Procesados.vue";
 </script>
 
 <template>
-    <Header />
-    <div class="row my-3">
-      <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <div id = "g_cabecera_adr">
-  
-                <a><button type="button" @click="opciones(1)" class="TableButtons"> Pendientes </button></a>
-                <a><button type="button" @click="opciones(2)" class="TableButtons"> Procesados  </button></a>
-          </div>
-    
-        <div id = g_cuerpo_adr >
-          
-          <Pendientes v-if="(change==1)"/>
-          <Procesados v-if="(change==2)"/>
-          
-        </div>
+  <div class="row my-3">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+      <div id="g_cabecera_adr">
+        <a
+          ><button
+            type="button"
+            @click="
+              filter = 'pending';
+              getBookings();
+            "
+            class="TableButtons"
+          >
+            Pendientes
+          </button></a
+        >
+      </div>
+
+      <div id="g_cuerpo_adr">
+        <AdminReservasFrame
+          v-for="booking in bookings"
+          v-bind:key="booking.id"
+          v-bind:booking="booking"
+        />
       </div>
     </div>
+  </div>
 
-    <Footer />
-    </template>
+  <Footer />
+</template>
 
-    <style>
-  
-  #g_cabecera_adr {    
+<script>
+import axios from "axios";
+
+export default {
+  name: "Admin Reservas",
+
+  components: {
+    AdminReservasFrame,
+  },
+
+  data() {
+    return {
+      bookings: [],
+      filter: "pending",
+      page: 1,
+      has_next: "",
+      has_previous: "",
+    };
+  },
+
+  methods: {
+    getBookings() {
+      const token = localStorage.getItem("authToken");
+      axios
+        .get(
+          "http://127.0.0.1:8001/booking/reservation?page=" +
+            this.page +
+            "&status=" +
+            this.filter,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.data) {
+            this.bookings = res.data.data;
+            this.has_next = res.data.has_next;
+            this.has_previous = res.data.has_previous;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+
+  mounted() {
+    this.getBookings();
+  },
+};
+</script>
+
+<style>
+#g_cabecera_adr {
   padding: 19px;
-  background-color: #A6A2A2 ;
+  background-color: #a6a2a2;
   width: 90%;
   margin: 0px auto;
   text-align: center;
-  }
+}
 
-  #g_cuerpo_adr {
-    background-color: #CFD2CD;    
-    border:3px solid #A6A2A2;
-    padding: 19px;
-    width: 90%;
-    margin: 0px auto;
-    border-bottom-right-radius: 5px;
-    border-bottom-left-radius: 5px;
-    }
-    #g_producto_adr {
-    background-color: #E5E6E4;
-    width: 100%;
-    
-    margin: 0px auto;
-    border-radius: 20px;
-    }
-     
-    #g_informacion_adr h1{
-      text-align: center;
-      color: #847577;
+#g_cuerpo_adr {
+  background-color: #cfd2cd;
+  border: 3px solid #a6a2a2;
+  padding: 19px;
+  width: 90%;
+  margin: 0px auto;
+  border-bottom-right-radius: 5px;
+  border-bottom-left-radius: 5px;
+}
+#g_producto_adr {
+  background-color: #e5e6e4;
+  width: 100%;
 
-    }
-  </style>
+  margin: 0px auto;
+  border-radius: 20px;
+}
 
-  
+#g_informacion_adr h1 {
+  text-align: center;
+  color: #847577;
+}
+</style>
--- src/components/Procesar_reservas.vue
+++ src/components/Procesar_reservas.vue
@@ -1,87 +1,184 @@
 <script setup>
-import Header from './Header.vue';
-import Footer from './Footer.vue';
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
 </script>
 
 <template>
-    <Header/>
-   <div class="row my-3 " id="edit_menu_pr">
-      <div class="col-md-6" id="edit_menu_pr" >
-        <h1>modificar item</h1>
+  <div class="row my-3" id="edit_menu_pr">
+    <div class="col-md-6" id="edit_menu_pr">
+      <h1>Procesar Reserva</h1>
 
-        <form class="  row my-5 ">
-
-            <div class="col-md-6 " >
-            <input type="text" class="form-control" placeholder="nombre completo: ">
-            </div>
-
-            <div class="col-md-6"  >
-            <input type="text" class="form-control" placeholder="documento de identidad: ">
-                </div>
-             <div class="col-md-6 " >
-            <input type="date" class="form-control" placeholder="fecha: ">
-            </div>
-            <div class="col-md-6 " >
-            <input type="time" class="form-control" placeholder="hora: ">
-            </div>
-            <div class="col-md-12 " >
-            <input type="text" class="form-control" placeholder="reserva empresarial: ">
-            </div>
-            <div class="col-md-6" >
-
-                <input type="submit" value="aceptar" class="form-control style-submit_pr">
-
-            </div>
-
-            <div class="col-md-6" >
-
-                <input type="submit" value="rechazar" class="form-control style-submit_pr">
-
-</div>
-</form>
-
+      <div class="row my-5" id="info">
+        <div class="col-md-6">{{ booking.names }} {{ booking.surnames }}</div>
+        <div class="col-md-6">2</div>
+        <div class="col-md-6">3</div>
+        <div class="col-md-6">4</div>
+        <div class="col-md-6">5</div>
+        <div class="col-md-6">6</div>
+        <div class="col-md-6">7</div>
+        <div class="col-md-6">8</div>
+        <div class="col-md-6">9</div>
       </div>
-</div>
+    </div>
+  </div>
 
-   <Footer/>
+  <Footer />
 </template>
 
+<script>
+import axios from "axios";
+import Swal from "sweetalert2";
+
+export default {
+  name: "Reservas",
+
+  data() {
+    return {
+      booking: [],
+    };
+  },
+
+  methods: {
+    makeFormData(processResult) {
+      return {
+        status: processResult,
+        observation: this.observation,
+      };
+    },
+
+    getBooking() {
+      axios
+        .get(
+          "http://127.0.0.1:8001/booking/reservation/" + this.$route.params.id
+        ) //ajustar la url en el futuro
+        .then((res) => {
+          this.booking = res.data;
+          console.log(this.booking);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+    processBooking() {
+      const token = localStorage.getItem("authToken");
+      Swal.fire({
+        icon: "info",
+        title: "Va a eliminar " + this.booking.document + ", ¿Está seguro?",
+        showCancelButton: true,
+        confirmButtonText: "Aceptar",
+        preConfirm: async () => {
+          axios
+            .patch(
+              "http://127.0.0.1:8001/booking/reservation" + this.$route.params.id,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            )
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              console.log(err.response);
+            });
+        },
+        allowOutsideClick: () => !Swal.isLoading(),
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            icon: "success",
+            title: `Reserva Aceptada satisfactoriamente`,
+          });
+          this.$router.push("/Administrador_rersevas");
+        }
+      });
+    },
+
+    patchProduct() {
+      const token = localStorage.getItem("authToken");
+      axios
+        .patch(
+          "http://127.0.0.1:8001/menu/product/" + this.$route.params.id,
+          this.makeFormData(),
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+          Swal.fire({
+            icon: "success",
+            title: `Producto editado satisfactoriamente`,
+          });
+          this.$router.push("/Administrador_menu");
+        })
+        .catch((err) => {
+          console.log(err.response);
+          Swal.fire({
+            icon: "error",
+            title: "Datos no válidos",
+          });
+        });
+    },
+  },
+
+  mounted() {
+    const token = localStorage.getItem("authToken");
+    axios
+      .get(
+        "http://127.0.0.1:8001/booking/reservation/" + this.$route.params.id,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      ) //ajustar la url en el futuro
+      .then((res) => {
+        this.booking = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
+</script>
+
 <style>
-     
-    #edit_menu_pr{
-        
-        margin: 0 auto;
-        width: 100%;        
+#edit_menu_pr {
+  margin: 0 auto;
+  width: 100%;
+}
 
-    }
+#edit_menu_pr h1 {
+  text-align: center;
+  background-color: #e5e6e4;
+  height: 60px;
+  border-radius: 20px;
+  width: 70%;
+  margin: 0px auto;
+  color: #847577;
+}
+#edit_menu_pr div #info {
+  margin: 0 auto;
+  width: 50%;
+}
+#edit_menu_pr p,
+select {
+  margin: 10px;
+}
 
-    #edit_menu_pr h1{
-      text-align: center;
-      background-color: #E5E6E4 ;
-      height: 60px;
-      border-radius: 20px;
-      width: 70%;
-      margin: 0px auto ;
-      color: #847577
-      
-    }
-    #edit_menu_pr form{
-        margin: 0 auto;
-        width: 50%;
-    }
-    #edit_menu_pr input, select{
-        margin: 10px;
-    }
+#posarea_pr {
+  margin-left: 23px;
+}
 
-    #posarea_pr{
-        margin-left: 23px
-    }
-    
-    .style-submit_pr{
-        color:#E5E6E4;
-        background-color: #A6A2A2;
-        height: 60px;
-        font-size: 30px;
-    }
-    
+.style-submit_pr {
+  color: #e5e6e4;
+  background-color: #a6a2a2;
+  height: 60px;
+  font-size: 30px;
+}
 </style>
